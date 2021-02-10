fa1d160e08f84fceff258dddb3adfead2d05da05
Victor Mathias Panzer Iversen <vmiverse1@gmail.com>
2021-02-10 10:22:49 +0100

    adds clock

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import DadJokes from "./DadJokes";
 import Bikes from "./Bikes";
+import Clock from "./Clock";
 
 function App() {
   return (
     <div className="App">
+      <Clock />
       <DadJokes />
       <Bikes />
     </div>
---
+++ src/Clock.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { DateTime } from "luxon";
+
+function Clock() {
+  const dateTime = DateTime.local().setLocale("nb-NO");
+
+  return (
+    <div className="time">
+      <h2 className="time">
+        {dateTime.toLocaleString(DateTime.TIME_24_SIMPLE)}
+      </h2>
+      <h2 className="date">
+        {dateTime.toLocaleString({
+          weekday: "long",
+          day: "numeric",
+          month: "long",
+        })}
+      </h2>
+    </div>
+  );
+}
+
+export default Clock;
